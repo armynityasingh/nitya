f6ae9c551e7732d9c6f086565547c10e7a62cab4
armynityasingh <armynityasingh@gmail.com>
2025-01-20 10:08:29 +0530

    odd checking

--- src/main/scala/practice.scala
+++ src/main/scala/practice.scala
@@ -7,11 +7,6 @@ object NumberCheck {
     val number = 7 // Example number
     if (isOddAndNotDivisibleBy3(number)) {
       println(s"$number is odd and not divisible by 3")
-    } else {
-      println(s"$number is either even or divisible by 3")
     }
-
-    // Run the main method to see the output
-    NumberCheck.main(Array())
   }
 }
