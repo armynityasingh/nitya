ff288f13ccf1aa5b73d8be7ef5d21b308951340b
armynityasingh <armynityasingh@gmail.com>
2025-01-20 10:14:27 +0530

    divisibility_chking

---
+++ src/main/scala/divisibility.scala
@@ -0,0 +1,14 @@
+object DivisibilityCheck {
+  def isDivisibleBy4Or6(num: Int): Boolean = {
+    num % 4 == 0 || num % 6 == 0
+  }
+
+  def main(args: Array[String]): Unit = {
+    val number = 18
+    if (isDivisibleBy4Or6(number)) {
+      println(s"$number is divisible by either 4 or 6")
+    } else {
+      println(s"$number is not divisible by either 4 or 6")
+    }
+  }
+}
